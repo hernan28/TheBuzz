e4b9c67e2719104ea4f74fd8d4b3038be28126b7
AllanCSR09 <allancs.electro@gmail.com>
2023-05-04 18:21:43 -0600

    User Insertion script and tests

---
+++ packages/db/defaultUsers/users.sql
@@ -0,0 +1,8 @@
+INSERT INTO "User"("name","lastName","userName","email","birthDate","description")
+VALUES ('Allan','Cespedes','AllanR09','allancs.electro@gmail.com', '1996-12-21','Anime lover');
+
+INSERT INTO "User"("name","lastName","userName","email","birthDate","description")
+VALUES ('Hernan','Madrigal','Herlock','hernan.madrigal28@gmail.com', '1997-04-28','Idiota');
+
+INSERT INTO "User"("name","lastName","userName","email","birthDate","description")
+VALUES ('Mauro','Rodriguez','Maurod','maurod.96@gmail.com', '1996-11-28','Sepsy');

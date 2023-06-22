1d5598e54b0065cb0b0c311752001ececc700fff
hernan28 <hernanmadrigal28@gmail.com>
2023-06-21 20:36:29 -0600

    Draft first screen

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,133 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from "react";
-import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  Image,
+  ImageBackground,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Stack, useRouter } from "expo-router";
-import { FlashList } from "@shopify/flash-list";
-
-import { api, type RouterOutputs } from "../utils/api";
-
-const PostCard: React.FC<{
-  post: RouterOutputs["post"]["all"][number];
-  onDelete: () => void;
-}> = ({ post, onDelete }) => {
-  const router = useRouter();
-
-  return (
-    <View className="flex flex-row rounded-lg bg-white/10 p-4">
-      <View className="flex-grow">
-        <TouchableOpacity onPress={() => router.push(`/post/${post.id}`)}>
-          <Text className="text-xl font-semibold text-pink-400">
-            {post.title}
-          </Text>
-          <Text className="mt-2 text-white">{post.content}</Text>
-        </TouchableOpacity>
-      </View>
-      <TouchableOpacity onPress={onDelete}>
-        <Text className="font-bold uppercase text-pink-400">Delete</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-const CreatePost: React.FC = () => {
-  const utils = api.useContext();
-
-  const [title, setTitle] = React.useState("");
-  const [content, setContent] = React.useState("");
-
-  const { mutate, error } = api.post.create.useMutation({
-    async onSuccess() {
-      setTitle("");
-      setContent("");
-      await utils.post.all.invalidate();
-    },
-  });
-
-  return (
-    <View className="mt-4">
-      <TextInput
-        className="mb-2 rounded bg-white/10 p-2 text-white"
-        placeholderTextColor="rgba(255, 255, 255, 0.5)"
-        value={title}
-        onChangeText={setTitle}
-        placeholder="Title"
-      />
-      {error?.data?.zodError?.fieldErrors.title && (
-        <Text className="mb-2 text-red-500">
-          {error.data.zodError.fieldErrors.title}
-        </Text>
-      )}
-      <TextInput
-        className="mb-2 rounded bg-white/10 p-2 text-white"
-        placeholderTextColor="rgba(255, 255, 255, 0.5)"
-        value={content}
-        onChangeText={setContent}
-        placeholder="Content"
-      />
-      {error?.data?.zodError?.fieldErrors.content && (
-        <Text className="mb-2 text-red-500">
-          {error.data.zodError.fieldErrors.content}
-        </Text>
-      )}
-      <TouchableOpacity
-        className="rounded bg-pink-400 p-2"
-        onPress={() => {
-          mutate({
-            title,
-            content,
-          });
-        }}
-      >
-        <Text className="font-semibold text-white">Publish post</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+import { Stack } from "expo-router";
 
 const Index = () => {
-  const utils = api.useContext();
-
-  const postQuery = api.post.all.useQuery();
-
-  const deletePostMutation = api.post.delete.useMutation({
-    onSettled: () => utils.post.all.invalidate(),
-  });
-
   return (
-    <SafeAreaView className="bg-[#1F104A]">
+    <SafeAreaView className="bg-[#000000]">
       {/* Changes page title visible on the header */}
-      <Stack.Screen options={{ title: "Home Page" }} />
-      <View className="h-full w-full p-4">
-        <Text className="mx-auto pb-2 text-5xl font-bold text-white">
-          Create <Text className="text-pink-400">T3</Text> Turbo
-        </Text>
-
-        <Button
-          onPress={() => void utils.post.all.invalidate()}
-          title="Refresh posts"
-          color={"#f472b6"}
-        />
-
-        <View className="py-2">
-          <Text className="font-semibold italic text-white">
-            Press on a post
-          </Text>
-        </View>
-
-        <FlashList
-          data={postQuery.data}
-          estimatedItemSize={20}
-          ItemSeparatorComponent={() => <View className="h-2" />}
-          renderItem={(p) => (
-            <PostCard
-              post={p.item}
-              onDelete={() => deletePostMutation.mutate(p.item.id)}
+      <Stack.Screen options={{ title: "The Buzz" }} />
+      <ImageBackground
+        source={require("../../assets/backgroundPattern.png")}
+        resizeMode="cover"
+      >
+        <Stack.Screen />
+        <View className="container h-full w-full">
+          <View className="h-200 w-200 pt-20">
+            <Image
+              source={require("../../assets/theBuzzLogo.png")}
+              alt="app logo"
+              className="mx-auto"
             />
-          )}
-        />
+          </View>
 
-        <CreatePost />
-      </View>
+          <View className="mx-auto w-full px-20 pt-20">
+            <View className="p-2">
+              <Text className="text-center font-semibold italic text-white">
+                Usuario
+              </Text>
+              <TextInput className="rounded-full bg-[#D9D9D9]/25 px-2 text-white"></TextInput>
+            </View>
+            <View className="p-2">
+              <Text className="text-center font-semibold italic text-white">
+                Contraseña asda
+              </Text>
+              <TextInput className="rounded-full bg-[#D9D9D9]/25 px-2 text-white"></TextInput>
+            </View>
+            <View className="pt-15">
+              <Button title="Listo" color="#CBA7EF" />
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
